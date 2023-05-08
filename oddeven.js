7bd296f3a7f3c27cb07177650a44cbb7ddbc1b86
SOORYAJITH-555 <132890038+SOORYAJITH-555@users.noreply.github.com>
2023-05-08 15:56:34 +0530

    Add files via upload

---
+++ oddeven.js
@@ -0,0 +1,13 @@
+let number = [2,3,5,6,8,11];
+for(let i=0;i<number.length;i++)
+{
+    if(number[i]%2==0)
+    {
+        console.log(number[i]," is even");
+    }
+    else
+    {
+        console.log(number[i]," is odd");
+    }
+
+}
